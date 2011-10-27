eb05e850cea59625f70e045ca48430aa0a52f091
Raphael Crawford-Marks <raphael.crawfordmarks@gmail.com>
2011-10-27 12:16:47 -0400

    Fixes for code review.

--- app/controllers/resource_categories_controller.rb
+++ app/controllers/resource_categories_controller.rb
@@ -1,20 +1,34 @@
 class ResourceCategoriesController < ApplicationController
 
-  before_filter :find_all_resource_categories
+  #before_filter :find_all_resource_categories
   before_filter :find_page
 
+  def index_employer
+    @audience = :employers
+    @resource_categories = ResourceCategory.for_audience(@audience.to_s)
+    render "resource_categories/index"
+  end
+  
+  def index_student
+    @audience = :students
+    @resource_categories = ResourceCategory.for_audience(@audience.to_s)
+    render "resource_categories/index"
+  end
+  
   def index
     # you can use meta fields from your model instead (e.g. browser_title)
     # by swapping @page for @resource_category in the line below:
+    
     present(@resource_category)
   end
 
   def show
     @resource_category = ResourceCategory.find(params[:id])
-
+    @audience = @resource_category.audience.to_sym
+    
     # you can use meta fields from your model instead (e.g. browser_title)
     # by swapping @page for @resource_category in the line below:
-    present(@resource_category)
+    #present(@resource_category)
   end
 
 protected
--- app/models/resource_category.rb
+++ app/models/resource_category.rb
@@ -6,8 +6,16 @@ class ResourceCategory < ActiveRecord::Base
   
   belongs_to :cover_image, :class_name => 'Image'
   belongs_to :resource_guide, :class_name => 'ResourceGuide'
+  has_many :resource_documents, :class_name => 'ResourceDocument'
   
   has_friendly_id :title, :use_slug => true
+  
+  #
+  # Scopes
+  #
+  #scope :for_audience,  lambda { |a| {:conditions => ["audience = ?", a]} }
+  scope :for_audience,  lambda { |a| where(:audience => a) }
+  
   
   def documents
     ResourceDocument.where(:resource_category_id => id)
